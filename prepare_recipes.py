f7ff3e42a8d600ad01e088b3152f3d36c81ffe2f
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-05 18:48:22 +0100

    Run script for all four packages

--- prepare_recipes.py
+++ prepare_recipes.py
@@ -1,14 +1,22 @@
 import importlib
 from jinja2 import Template
 
+
 def prepare_recipe(package):
 
-    with open('recipes/{0}/meta.yaml') as f:
+    with open('recipes/{0}/meta.yaml'.format(package)) as f:
         content = f.read()
 
     mod = importlib.import_module(package)
 
     recipe = Template(content).render(version=mod.__version__)
 
-    with open('recipes/{0}/meta.yaml', 'w') as f:
+    with open('recipes/{0}/meta.yaml'.format(package), 'w') as f:
         f.write(recipe)
+
+
+if __name__ == "__main__":
+    prepare_recipe('glue')
+    prepare_recipe('glue-vispy-viewers')
+    prepare_recipe('glue-medical')
+    prepare_recipe('glue-geospatial')
