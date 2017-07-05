26ce228bbfd91469b6b6050f926049551458d6f8
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-05 19:08:37 +0100

    Include real dev version in conda versions

--- prepare_recipes.py
+++ prepare_recipes.py
@@ -11,15 +11,18 @@ def prepare_recipe(package):
         content = f.read()
 
     os.chdir(package)
+    overall_version = subprocess.check_output('python setup.py --version', shell=True).decode('ascii')
     utime = subprocess.check_output('git log -1 --pretty=format:%ct', shell=True).decode('ascii')
     chash = subprocess.check_output('git log -1 --pretty=format:%h', shell=True).decode('ascii')
     os.chdir('..')
 
     stamp = datetime.fromtimestamp(int(utime)).strftime('%Y%m%d%H%M%S')
 
-    recipe = Template(content).render(version='dev.' + stamp + '.' + chash)
+    overall_version = overall_version.strip().split('.dev')[0]
 
-    print(recipe)
+    version = overall_version + '.dev' + stamp + '.' + chash
+
+    recipe = Template(content).render(version=version)
 
     with open('recipes/{0}/meta.yaml'.format(package), 'w') as f:
         f.write(recipe)
@@ -28,5 +31,6 @@ def prepare_recipe(package):
 if __name__ == "__main__":
     prepare_recipe('glue-core')
     prepare_recipe('glue-vispy-viewers')
+    prepare_recipe('glueviz')
     #prepare_recipe('glue-medical')
     #prepare_recipe('glue-geospatial')
