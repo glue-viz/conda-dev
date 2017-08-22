f83aa8be39d8b7405dca67cb3987b25efbf1dfd5
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-22 22:10:26 +0100

    Started adding support for building stable versions

--- build_packages.sh
+++ build_packages.sh
@@ -12,26 +12,44 @@ conda install jinja2 pyqt
 # Don't auto-upload, instead we upload manually specifying a token.
 conda config --set anaconda_upload no
 
-packages="glue-core glue-vispy-viewers glueviz glue-wwt glue-geospatial"
+if [[ $TRAVIS_BRANCH == stable ]]; then
+  packages="glue-core glue-vispy-viewers glueviz";
+else
+  packages="glue-core glue-vispy-viewers glueviz glue-wwt glue-geospatial";
+fi
 
 for package in $packages; do
 
-  if [[ $package == glue-core ]]; then
-    git clone git://github.com/glue-viz/glue.git glue-core
-  else
-    git clone "git://github.com/glue-viz/"$package".git"
-  fi
+  if [[ $TRAVIS_BRANCH == stable ]]; then
 
-  # The following puts the correct version number in the recipes
-  python prepare_recipe.py $package
+    # The following puts the correct version number and md5 in the recipes
+    python prepare_recipe.py $package --stable;
+
+  else
+
+    if [[ $package == glue-core ]]; then
+      git clone git://github.com/glue-viz/glue.git glue-core;
+    else
+      git clone "git://github.com/glue-viz/"$package".git"
+    fi
+
+    # The following puts the correct version number in the recipes
+    python prepare_recipe.py $package;
+
+  fi
 
   cd recipes
 
   conda build --python $PYTHON_VERSION $package
   output=`conda build --python $PYTHON_VERSION $package --output`
   test -e $output
+
   if [[ $TRAVIS_EVENT_TYPE != pull_request && $TRAVIS_BRANCH == master ]]; then
     anaconda -t $ANACONDA_TOKEN upload --force -l dev $output;
+  fi
+
+  if [[ $TRAVIS_EVENT_TYPE != pull_request && $TRAVIS_BRANCH == stable ]]; then
+    anaconda -t $ANACONDA_TOKEN upload $output;
   fi
 
   cd ..
