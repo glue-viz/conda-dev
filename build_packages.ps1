318a6305cbbcfe06a5e3d125153478645e0d7696
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-04-25 23:33:08 +0100

    Try and fix syntax

--- build_packages.ps1
+++ build_packages.ps1
@@ -76,10 +76,10 @@ foreach ($package in $packages) {
   # If we are processing a pull request, we shouldn't skip builds even if they
   # exist already otherwise some builds might not get tested
   if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
-    skip = "--skip-existing"
+    $skip = "--skip-existing"
     checkLastExitCode
   } else {
-    skip = ""
+    $skip = ""
     checkLastExitCode
   }
 
