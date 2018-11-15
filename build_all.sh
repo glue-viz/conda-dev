2440059573d0bc9893c840659482b089485a0b32
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-11-15 10:42:19 +0000

    Build Python 3.6 packages first to get all noarch dependencies in place

--- build_all.sh
+++ build_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
+# NOTE: start with Python 3.6 to get noarch packages built first
+
 export STABLE=true
+
+export PYTHON_VERSION=3.6
+./build_packages.sh
 
 export PYTHON_VERSION=2.7
 ./build_packages.sh
 
 export PYTHON_VERSION=3.5
-./build_packages.sh
-
-export PYTHON_VERSION=3.6
 ./build_packages.sh
 
 export PYTHON_VERSION=3.7
@@ -16,13 +18,13 @@ export PYTHON_VERSION=3.7
 
 export STABLE=false
 
+export PYTHON_VERSION=3.6
+./build_packages.sh
+
 export PYTHON_VERSION=2.7
 ./build_packages.sh
 
 export PYTHON_VERSION=3.5
-./build_packages.sh
-
-export PYTHON_VERSION=3.6
 ./build_packages.sh
 
 export PYTHON_VERSION=3.7
