7eb7d38dc0974d324b363bb16e75b1f3facd7ea6
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-18 16:08:40 +0100

    Add glue-plotly to packages to be built

--- build_packages.sh
+++ build_packages.sh
@@ -42,7 +42,7 @@ fi
 # noarch packages are only build on Python 3.6 on CircleCI
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" ]]; then
-  packages+=" specviz mosviz glue-geospatial glue-samp glue-vispy-viewers glue-wwt";
+  packages+=" glue-plotly specviz mosviz glue-geospatial glue-samp glue-vispy-viewers glue-wwt";
 fi
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" && $STABLE == false ]]; then
