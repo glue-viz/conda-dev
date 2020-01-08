dff47d56649bf18cbfc79a60d8945a14b86ab3ef
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-08 19:00:17 +0000

    Update metadata for glue-core, glue-jupyter, and glue-vispy-viewers, and enable stable build for glue-jupyter

--- build_packages.sh
+++ build_packages.sh
@@ -42,11 +42,11 @@ fi
 # noarch packages are only build on Python 3.6 on CircleCI
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" ]]; then
-  packages+=" glue-plotly specviz mosviz glue-geospatial glue-samp glue-vispy-viewers glue-wwt";
+  packages+=" glue-jupyter glue-plotly specviz mosviz glue-geospatial glue-samp glue-vispy-viewers glue-wwt";
 fi
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" && $STABLE == false ]]; then
-  packages+=" glue-openspace bqplot ipymaterialui glue-jupyter glue-regions glue-exp cubeviz";
+  packages+=" glue-openspace bqplot ipymaterialui glue-regions glue-exp cubeviz";
 fi
 
 # This needs to be built after glue-vispy-viewers
