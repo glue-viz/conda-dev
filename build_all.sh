d38e210894eb23b2f358c799b4b11d8e2395fa9f
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-08 19:43:51 +0000

    Drop Python 2.7 and 3.5

--- build_all.sh
+++ build_all.sh
@@ -7,24 +7,12 @@ export STABLE=true
 export PYTHON_VERSION=3.6
 ./build_packages.sh
 
-export PYTHON_VERSION=2.7
-./build_packages.sh
-
-export PYTHON_VERSION=3.5
-./build_packages.sh
-
 export PYTHON_VERSION=3.7
 ./build_packages.sh
 
 export STABLE=false
 
 export PYTHON_VERSION=3.6
-./build_packages.sh
-
-export PYTHON_VERSION=2.7
-./build_packages.sh
-
-export PYTHON_VERSION=3.5
 ./build_packages.sh
 
 export PYTHON_VERSION=3.7
