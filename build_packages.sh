a0e74adf23ecaa0977e259ae5b59cfaddd4651f4
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-09-18 10:31:52 +0100

    Build glue-astronomy

--- build_packages.sh
+++ build_packages.sh
@@ -39,7 +39,7 @@ fi
 # noarch packages are only build on Python 3.6 on CircleCI
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" ]]; then
-  packages+=" glue-vispy-viewers glue-jupyter glue-plotly specviz mosviz glue-geospatial glue-samp glue-wwt";
+  packages+=" glue-vispy-viewers glue-astronomy glue-jupyter glue-plotly specviz mosviz glue-geospatial glue-samp glue-wwt";
 fi
 
 if [[ $PYTHON_VERSION == "3.6" && $CIRCLE_SHA1 != "" && $STABLE == false ]]; then
